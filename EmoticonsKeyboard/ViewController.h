88be2edc91e26d7d001691ab6bba9e7156f1e939
duongnguyen <duongnguyenhuu@vccorp.vn>
2016-03-12 12:44:02 +0700

    Initial Commit

---
+++ EmoticonsKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmoticonsKeyboard
+//
+//  Created by duongnguyen on 3/12/16.
+//  Copyright © 2016 carpediem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
